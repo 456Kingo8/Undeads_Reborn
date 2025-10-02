8161b6dc0f98d40dfc2892af1cb4bb6b5f6f54f8
King08 <142666224+456Kingo8@users.noreply.github.com>
2025-10-02 18:17:08 +0800

    Add file

---
+++ Code/Undead_Trait.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModTemplate.Code
+{
+     class Undead_Trait
+     {
+        public static void init()
+        {
+
+            return;
+        }
+     }
+}
