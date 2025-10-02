8dbc3910046e5f4d322f66476a45d132bd49c634
King08 <142666224+456Kingo8@users.noreply.github.com>
2025-10-02 18:20:56 +0800

    Add files

---
+++ Undeads_Reborn.cs
@@ -0,0 +1,17 @@
+﻿using ModTemplate.Code;
+using NeoModLoader.api;
+
+namespace Undeads_Reborn
+{
+    public class Undeads_Reborn : BasicMod<Undeads_Reborn>
+    {
+        protected override void OnModLoad()
+        {
+            // Load your mod here
+            // 加载你的mod内容
+            Undead_Trait.init();
+            // LogInfo(GetConfig()["Default"]["WhatToSay"].TextVal); // Call this only then you confirm it is a text config item
+            LogInfo(GetConfig()["Default"]["WhatToSay"].GetValue() as string);
+        }
+    }
+}
